3ca606e0eb33fe5c9a19b258e797f95db3ed24bc
Stanislav Malyshev <smalyshev@gmail.com>
2019-07-22 22:49:17 -0700

    Fix parser fuzzer

--- fuzzer-parser.c
+++ fuzzer-parser.c
@@ -73,8 +73,8 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 	file_handle.handle.stream.fsizer = NULL;
 	file_handle.handle.stream.isatty = 0;
 	file_handle.handle.stream.closer   = NULL;
-	file_handle.handle.stream.buf = s;
-	file_handle.handle.stream.len = Size;
+	file_handle.handle.buf = s;
+	file_handle.handle.len = Size;
 
 	//fuzzer_do_parse(&file_handle, "fuzzer.php");
 	fuzzer_do_request(&file_handle, "fuzzer.php");
